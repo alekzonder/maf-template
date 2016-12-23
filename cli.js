f034f92e8acb519b7e93169453fe5949756ec28c
alekzonder <alekzonder@gmail.com>
2016-12-23 07:14:42 +0700

    new: maf/Init/Cli integration

--- cli.js
+++ cli.js
@@ -1,64 +1,17 @@
-const repl = require('repl-extra');
-
 var logger = require('maf/Service/Logger')('maf-cli');
 
-process.on('unhandledRejection', function (err) {
-    logger.fatal(err);
-});
+var initCli = require('maf/Service/Init/Cli');
 
-require('./init/config')(logger)
-    .then((config) => {
-        return require('./init/di')(logger, config);
-    })
-    .then((di) => {
-        var server = repl.startExtra({prompt: '# '});
+var init = {
+    config: require('./init/config'),
+    di: require('./init/di')
+};
 
-        server.context.i = function (data, depth) {
-            if (typeof depth === 'undefined') {
-                depth = 1;
-            }
-
-            console.log(require('util').inspect(data, {
-                showHidden: false,
-                colors: false,
-                depth: depth
-            }));
-
-            return '';
-        };
-
-        server.context.di = di;
-
-        server.context.a = di.api;
-        server.context.m = di.models;
-
-        server.context.methods = function (obj) {
-            return Object.getOwnPropertyNames(obj).filter(function (p) {
-                return typeof obj[p] === 'function';
-            });
-        };
-
-        server.context.ensureIndexes = function () {
-            return server.context.m.ensureIndexes();
-        };
-
-        server.context.createTestData = function () {
-
-            return new Promise((resolve, reject) => {
-                var api = server.context.a;
-
-                api.createTest()
-                .then(() => {
-                    resolve('done');
-                })
-                .catch((error) => {
-                    reject(error);
-                });
-            });
-
-        };
-
+initCli(logger, init)
+    .then(() => {
+        logger.debug('repl started');
     })
     .catch((error) => {
-        logger.fatal(error);
+        logger.error(error);
+        process.exit(1);
     });
